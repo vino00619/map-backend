53cd1c0e6b08f338838f9bd156685e14ec96f479
vino00619 <vinoth00619@gmail.com>
2022-08-17 19:21:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/users")
+const pinRoute = require("./routes/pins")
+const app = express();
+const cors = require('cors');
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("mongoDB connected")
+})
+.catch((err)=>console.log(err));
+//.
+
+
+app.use(cors())
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+
+app.listen(process.env.PORT || 8800, () => {
+    console.log("server is running");
+})
