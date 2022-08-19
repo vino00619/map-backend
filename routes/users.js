8ee4163013d769c4fce161a2ad51d3a212093134
vino00619 <vinoth00619@gmail.com>
2022-08-19 10:43:20 +0530

    dgh

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 //const Pin = require("../models/Pin");
+//comment added
 const bcrypt = require("bcrypt");
 
 //register
