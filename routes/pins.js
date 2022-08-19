11269098dc5e511f02c41cc46f9a28dd6ba1c37c
vino00619 <vinoth00619@gmail.com>
2022-08-19 11:53:18 +0530

    mmm

--- routes/pins.js
+++ routes/pins.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res)=>{
 router.get("/", async (req, res)=>{
     try{
         const pins = await Pin.find();
-        res.send({message:pins});
+        // res.send({message:pins});
         res.status(200).json(pins);
     }catch(err){
         res.status(500).json(err);
