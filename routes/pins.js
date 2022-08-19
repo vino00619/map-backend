bddf1e70d78f8ca4fa54bdc7c4577ef4f4f3787c
vino00619 <vinoth00619@gmail.com>
2022-08-19 11:33:15 +0530

    gg

--- routes/pins.js
+++ routes/pins.js
@@ -7,6 +7,7 @@ router.post("/", async (req, res)=>{
     const newPin = new Pin(req.body);
     try{
         const savedPin = await newPin.save();
+        res.send({message:savedPin});
         res.status(200).json(savedPin)
     } catch(err){
         res.status(500).json(err)
