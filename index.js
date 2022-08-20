120c74b68d9e9d41795f9320bdda037b8b34837a
vino00619 <vinoth00619@gmail.com>
2022-08-20 13:22:48 +0530

    qaz

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express")
 require("./config")
+const express = require("express")
 // const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/users")
