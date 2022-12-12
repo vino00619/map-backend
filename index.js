1e2d1f6039d6ba8625f9c0cff97e2f01dc1e167f
vino00619 <vinoth00619@gmail.com>
2022-12-12 23:16:17 +0530

    lxss

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const userRoute = require("./routes/users")
 const pinRoute = require("./routes/pins")
 const app = express();
 const cors = require('cors');
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    next(); 
-})
+// app.use((req,res,next)=>{
+//     res.setHeader('Access-Control-Allow-Origin','*');
+//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+//     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+//     next(); 
+// })
 dotenv.config();
 
 app.use(express.json());
@@ -26,7 +26,7 @@ app.use(express.json());
 
 
 
-// app.use(cors());
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Backend for map-bakcend running");
   });
