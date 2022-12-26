583b06f5f6258f5cbeea7a98725addb4e9f1b149
vino00619 <vinoth00619@gmail.com>
2022-12-26 13:00:12 +0530

    mongoose check

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("./config")
 const express = require("express")
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/users")
 const pinRoute = require("./routes/pins")
