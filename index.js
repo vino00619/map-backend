57d93461b11f20d5de73d902e83332f62ae5a12e
vino00619 <vinoth00619@gmail.com>
2023-01-03 22:11:47 +0530

    cors origin changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pinRoute = require("./routes/pins")
 const app = express();
 const cors = require('cors');
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','https://travel-stories-app.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
