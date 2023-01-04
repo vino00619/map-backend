c9980331e5a398b34c51e323311b1c10748669cd
vino00619 <vinoth00619@gmail.com>
2023-01-05 00:15:23 +0530

    rrr

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const userRoute = require("./routes/users")
 const pinRoute = require("./routes/pins")
 const app = express();
 const cors = require('cors');
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin','*');
-//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-//     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next(); 
-// })
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next(); 
+})
 app.use(cors());
 console.log("cors updated");
 // const corsOptions ={
